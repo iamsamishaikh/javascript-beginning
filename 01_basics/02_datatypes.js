d7274b72e5968bfc99f26c86814555feb4f7a30e
MD SAMIULLAH <contact.samishaikh@gmail.com>
2023-06-02 03:01:29 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+// https://tc39.es/ecma262/#sec-identifier-names // use this website for official documentation of js
+// as a beginner we are going to use MDN as our documentation
+"Use Strict"; // it treats all js code in newer version
+// alert(3+3) // we are using node js not browser
+console.log(3+3);
+console.log("Samiullah"); // readability should be preferred
+
+let name = "Samiullah"
+let age = 20
+let isLoggedin = false
+let state
+
+console.log(typeof age);
