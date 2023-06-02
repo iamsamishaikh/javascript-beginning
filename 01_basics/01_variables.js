ed99b1af9d1827e32183b0f1fc97bff8186066f3
MD SAMIULLAH <contact.samishaikh@gmail.com>
2023-06-02 02:23:34 +0000

    config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,8 @@
-const accountId = 1234
+const accountId = 1234  
+// we use const variable for the values we are not going to change in CSSFontFeatureValuesRule. and it will not change
 let accountEmail= "Samishaikh573@gmail.com"
 var accountPassword = "@thesam2222"
 // prefer not to use var variables as it is not effective some times
 console.table([accountId, accountPassword, accountEmail])
 // in js if we can not declare any variable type, there is nothing going to happen. but it is preferred that you have to use is 
-continue writing whatever is
+// continue writing whatever is
