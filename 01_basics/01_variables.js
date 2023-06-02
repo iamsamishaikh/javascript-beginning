5f709b61c0ce90a6bc8eb72df029f75eb9f42235
MD SAMIULLAH <contact.samishaikh@gmail.com>
2023-06-02 02:18:01 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 1234
+let accountEmail= "Samishaikh573@gmail.com"
+var accountPassword = "@thesam2222"
+// prefer not to use var variables as it is not effective some times
+console.table([accountId, accountPassword, accountEmail])
+// in js if we can not declare any variable type, there is nothing going to happen. but it is preferred that you have to use is 
+continue writing whatever is
