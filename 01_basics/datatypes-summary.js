712121ca0dd9d47995f6fe9cb4da653c1f5bcd46
MD SAMIULLAH <contact.samishaikh@gmail.com>
2023-06-05 12:46:18 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,21 @@
+// primitive data types :
+// types : 7 -- bigInt, string, number, boolean, symbol, undefined, null 
+
+      // reference or non- primitive data types
+      // arrays, functions and Objects 
+
+      let myfunctions = function name(params) {
+        name = sami 
+        age = 20
+        sex = male
+        
+      }
+      console.log("myfunctions");
+
+      const age = 99
+      let name;
+      let job = null
+      console.log(typeof age);
+      console.log(typeof name);
+
+      // read official documentation for better understanding 
