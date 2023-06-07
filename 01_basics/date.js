547fdba5c8952cef1f0044fe444cfb3a749b4d71
MD SAMIULLAH <contact.samishaikh@gmail.com>
2023-06-07 19:02:32 +0000

    date in js

---
+++ 01_basics/date.js
@@ -0,0 +1,15 @@
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate);
+
+// let createdDate = new Date(2023, 0, 13);
+
+// let date1 = new VarDate
+// console.log(date1);
+
+// let myDateStamp = date.now()
+// console.log(myDateStamp);
+
+
+myD = Date()
+console.log(myD);
